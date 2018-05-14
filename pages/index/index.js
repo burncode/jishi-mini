6c7ac60da034620051b4fafc9d6bcafcc52018e8
tang86 <80330582@163.com>
2018-05-14 23:54:44 +0800

    小程序页面

---
+++ pages/index/index.js
@@ -0,0 +1,21 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      '/demo/swiper.gif',
+      '/demo/swiper-002.gif'
+
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+
+  onLoad: function () {
+
+  }
+})
