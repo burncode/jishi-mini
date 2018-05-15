30ce4fb545a28d93b20125e3ae2af98beb78e702
guoqing <957534377@qq.com>
2018-05-15 09:46:04 +0800

    修改登录

--- app.js
+++ app.js
@@ -1,39 +1,55 @@
 //app.js
+const host = 'https://admin.gq1994.top/api'
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+    onLaunch: function () {
+        // 展示本地存储能力
+        var logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
+        let This = this
+        // 小程序打开
+        wx.login({
+            success: function (res) {
+                var code = res.code;
+                if (res.code) {
+                    wx.getUserInfo({
+                        withCredentials: true,
+                        success: res => {
+                            This.globalData.userInfo = res.userInfo;
+                            console.log(res.userInfo)
+                            console.log(code)
+                        },
+                        complete: res => {
+                            wx.request({
+                                url: getApp().globalData.login.url,
+                                data: {
+                                    js_code: code,
+                                    name: This.globalData.userInfo.nickName,
+                                    head_url: This.globalData.userInfo.avatarUrl,
+                                },
+                                method: getApp().globalData.login.method,
+                                success: function (res) {
+                                    if (res.data.StatusCode === 200) {
+                                        // 登录成功 将token存入本地
+                                        getApp().globalData._token = res.data.ResultData._token;
+                                        getApp().globalData.userInfo.id = res.data.ResultData.user.id;
+                                        getApp().globalData.userInfo.tel = res.data.ResultData.user.tel;
+                                    }
+                                }
+                            });
+                        }
+                    })
+                }
             }
-          })
-        }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
+        })
+    },
+    globalData: {
+        userInfo: {},
+        _token: null,
+        host: host,
+        login: {
+            url:host + '/login',
+            method:'post'
+        },
+    }
 })
