2b5bc346907723f523b412831f9011803039bebc
zhaochang <80330582@qq.com>
2018-06-01 11:02:32 +0800

    收集用户信息页面，地址默认为空

--- pages/ceping/user-form.js
+++ pages/ceping/user-form.js
@@ -31,12 +31,9 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-      console.log('已知用户信息：');
-      console.log(getApp().globalData.userInfo);
+      
         this.setData(getApp().globalData.userInfo);
-        // 载入时要显示再隐藏一下才能显示数据，如果有解决方法可以在issue提一下，不胜感激:-)
-        // 初始化数据
-        this.setAreaData()
+
     },
     setAreaData: function (p, c, d) {
       var p = p || 0 // provinceSelIndex
@@ -86,6 +83,9 @@ Page({
       this.setAreaData(p, c, d)
     },
     showDistpicker: function () {
+      // 载入时要显示再隐藏一下才能显示数据，如果有解决方法可以在issue提一下，不胜感激:-)
+      // 初始化数据
+      this.setAreaData()
       this.setData({
         showDistpicker: true
       })
